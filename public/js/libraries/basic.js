a6c9e35ca3ae6434316f047cbad2ed24b0813d10
Dmitry Poponkin <mitia.poponkin@yandex.ru>
2018-03-15 17:24:49 +0300

    function to validate query selectors. isQuerySelector
    Signed-off-by: Dmitry Poponkin <mitia.poponkin@yandex.ru>

--- public/js/libraries/basic.js
+++ public/js/libraries/basic.js
@@ -6,3 +6,11 @@ function reduceWithValues(fromArray, keyArray) {
         return allFields;
     }, {});
 }
+
+function isQuerySelector(querySelector) {
+    const elem = document.querySelector(querySelector);
+    if (elem === undefined) {
+        throw Error("no element found by query selector" + querySelector);
+    }
+    return elem;
+}
