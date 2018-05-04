57a35f2c80d47fc1d814c11ddd1b12cf45a0e6a7
Thewhiterabbit123 <nivasilieva13@gmail.com>
2018-05-04 17:45:15 +0300

    + background animation

--- public/jsMvc/Components/BackgroundAnimation/backgroundAnimation.js
+++ public/jsMvc/Components/BackgroundAnimation/backgroundAnimation.js
@@ -1,30 +1,36 @@
 class BackgroundAnimation {
     constructor() {
-        // const line = document.createElement('div');
-        // line.classList.add('background-animation');
-        // const parentEl = document.getElementsByClassName('js-wrapper-block');
-        // const positionX = Math.floor(Math.random() * parentEl.offsetX);
-        // line.left = `${positionX}px`;
+        this.colors = ['#e040fb', '#9c23c7', '#2e1e2e', '#ffc107', '#ffa000', '#2e2e02', '#689f38', '#8bc34a', '#1e2e22', '#e64a19', '#ff5722', '#391409', '#303f9f', '#3f51b5', '#10142b'];
         document.addEventListener('click', (evt) => {
             const line = document.createElement('div');
-            //const lineBack = document.createElement('div');
-            //lineBack.classList.add('background-animation');
-            // line.classList.add('background-animation');
-            const parentEl = document.getElementsByClassName('js-wrapper-block')[0];
+            line.classList.add('background-animation');
+            const parentEl = document.getElementsByClassName('js-application')[0];
             const positionX = (evt.pageX)? evt.pageX : evt.targetTouches[0].pageX;
             line.style.left = `${positionX}px`;
-            //line.style.left = `${positionX + Math.floor(Math.random())}px`;
-            parentEl.appendChild(line);
-            //parentEl.appendChild(lineBack);
+            line.style.backgroundColor = this.colors[Math.floor(Math.random() * this.colors.length)];
+            parentEl.insertAdjacentElement('afterbegin', line);
             line.addEventListener('animationend', () => {
                 line.remove();
-                //lineBack.remove();
             });
         });
+
+        window.requestAnimationFrame(this.createElementOnTimer.bind(this));
     }
 
     createElementOnTimer() {
-
+        setTimeout(() => {
+            const line = document.createElement('div');
+            line.classList.add('background-animation');
+            const parentEl = document.getElementsByClassName('js-application')[0];
+            const positionX = Math.floor(Math.random() * parentEl.offsetWidth);
+            line.style.left = `${positionX}px`;
+            parentEl.insertAdjacentElement('afterbegin', line);
+            line.style.backgroundColor = this.colors[Math.floor(Math.random() * this.colors.length)];
+            line.addEventListener('animationend', () => {
+                line.remove();
+            });
+            window.requestAnimationFrame(this.createElementOnTimer.bind(this));
+        }, Math.floor(Math.random() * 10)*1000);
     }
 }
 export {BackgroundAnimation};
