1f4c4390e9f42e971e7dc75f6bd85accd693d809
Dmitry Poponkin <mitia.poponkin@yandex.ru>
2018-05-04 03:26:33 +0300

    + added multiplayer Controller and Model functionality
    Signed-off-by: Dmitry Poponkin <mitia.poponkin@yandex.ru>

--- public/jsMvc/Controller/gameOnlineController.js
+++ public/jsMvc/Controller/gameOnlineController.js
@@ -1,65 +1,112 @@
 /**
  * @module controller/gameOnlineController
  */
-import {ws} from '../Modules/ws.js';
-import {CLIENT_EVENTS} from '../Modules/game/serverEvents.js';
+
+
 import {SERVER_EVENTS} from '../Modules/game/serverEvents.js';
-import {bus} from '../Modules/bus.js';
 import {OnlineGameModel} from '../Models/game/onlineGameModel.js';
 import {OnlineGameView} from '../Views/OnlineGameView/onlineGameView.js';
 import template from './../Views/OnlineGameView/onlineGameView.tmpl.xml';
 
-
+/**
+ * Class implements online game strategy (multiPlayer) controller.
+ */
 class OnlineGameController {
+
+    /**
+     * Initialize online game controller.
+     */
     constructor() {
         this.gameView = new OnlineGameView(template);
         this.gameModel = new OnlineGameModel();
-        this.ws = ws;
 
-        this.onGameStarted = this.onGameStarted.bind(this);
-        this.onCubicTaken = this.onCubicTaken.bind(this);
+        this.gameView.onReady = this.onReady.bind(this);
+        this.gameView.onSetCubic = this.onSetCubic.bind(this);
+        this.gameView.onClose = this.onClose.bind(this);
+
+        this.onStartGame = this.onStartGame.bind(this);
         this.onCubicSet = this.onCubicSet.bind(this);
-        this.onUpdateScore = this.onUpdateScore.bind(this);
+        this.onCubicDrop = this.onCubicDrop.bind(this);
         this.onEndGame = this.onEndGame.bind(this);
     }
 
-    startGame() {
-        bus.on(SERVER_EVENTS.START_GAME, this.onGameStarted);
-        bus.on(SERVER_EVENTS.CUBIC_TAKEN, this.onCubicTaken);
-        bus.on(SERVER_EVENTS.CUBIC_SET, this.onCubicSet);
-        bus.on(SERVER_EVENTS.UPDATE_SCORE, this.onUpdateScore);
-        bus.on(SERVER_EVENTS.END_GAME, this.onEndGame);
-
-
-        ws.send(CLIENT_EVENTS.READY, null);
+    /**
+     * Initialize handlers for server events.
+     */
+    initGame() {
+        this.gameModel.subscribe(SERVER_EVENTS.START_GAME, this.onStartGame);
+        this.gameModel.subscribe(SERVER_EVENTS.CUBIC_SET, this.onCubicSet);
+        this.gameModel.subscribe(SERVER_EVENTS.CUBIC_DROP, this.onCubicDrop);
+        this.gameModel.subscribe(SERVER_EVENTS.END_GAME, this.onEndGame);
     }
 
-    endGame() {
-        bus.off(SERVER_EVENTS.START_GAME, this.onGameStarted);
-        bus.off(SERVER_EVENTS.CUBIC_TAKEN, this.onCubicTaken);
-        bus.off(SERVER_EVENTS.CUBIC_SET, this.onCubicSet);
-        bus.off(SERVER_EVENTS.UPDATE_SCORE, this.onUpdateScore);
-        bus.off(SERVER_EVENTS.END_GAME, this.onEndGame);
+    /**
+     * Drop handlers from server events.
+     */
+    closeGame() {
+        this.gameModel.unsubscribe(SERVER_EVENTS.START_GAME, this.onStartGame);
+        this.gameModel.unsubscribe(SERVER_EVENTS.CUBIC_SET, this.onCubicSet);
+        this.gameModel.unsubscribe(SERVER_EVENTS.CUBIC_DROP, this.onCubicDrop);
+        this.gameModel.unsubscribe(SERVER_EVENTS.END_GAME, this.onEndGame);
     }
 
-    onGameStarted() {
-
+    /**
+     * Initialize map, players, render view.
+     *
+     * @param payload Contains map and info about the opponent.
+     */
+    onStartGame(payload) {
+        this.gameView.startGame(payload);
     }
 
-    onCubicTaken() {
-
+    /**
+     * Fix the cubic in place the player set it. Also renew the scores.
+     * @param {{color: string, your: number, opponent: number}} payload The info about cubic to be set, new players scores.
+     */
+    onCubicSet(payload) {
+        this.gameView.cubicSet(payload);
     }
 
-    onCubicSet() {
-
+    /**
+     * Drop the cubic from the place the player set it to the pool.
+     * @param {{color: string}} payload The info about cubic to be dropped to the pool.
+     */
+    onCubicDrop(payload) {
+        this.gameView.cubicDrop(payload);
     }
 
-    onUpdateScore() {
-
+    /**
+     * End the game in view.
+     * @param {{your: string, opponent: string, result: string}} payload The info about the scores and game winner.
+     */
+    onEndGame(payload) {
+        this.gameView.endGame(payload);
+        this.closeGame();
     }
 
-    onEndGame() {
+    /**
+     * Wire the signals from server and send to the server info that the player is searching for an opponent.
+     */
+    onReady() {
+        this.initGame();
+        this.gameModel.ready();
+    }
 
+    /**
+     * Send to the server info about placing the cube.
+     * Cube is not fixed yet. It waits for response.
+     * @param {{color: string, x: number, y: number}} payload - Info about where and which cube was set.
+     */
+    onSetCubic(payload) {
+        this.gameModel.setCubic(payload);
+    }
+
+    /**
+     * Send to the server info that the game was closed and destruct the wires connects server events with handlers.
+     */
+    onClose() {
+        this.gameModel.close();
+        this.closeGame();
     }
 }
 
