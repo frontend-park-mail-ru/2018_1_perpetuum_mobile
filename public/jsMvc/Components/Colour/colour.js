3e574d47c9fccb204eb5051831e43b2401f9695d
Thewhiterabbit123 <nivasilieva13@gmail.com>
2018-05-24 16:18:46 +0300

    + new color scheme

--- public/jsMvc/Components/Colour/colour.js
+++ public/jsMvc/Components/Colour/colour.js
@@ -17,13 +17,14 @@ class Colour {
      */
     constructor(rootClassName) {
         this.colorSet = {
-            baseColorPurple: ['#e040fb', '#9c23c7', '#2e1e2e'],
-            baseColorYellow: ['#ffc107', '#ffa000', '#2e2e02'],
-            baseColorGreen:  ['#689f38', '#8bc34a', '#1e2e22'],
-            baseColorOrange: ['#e64a19', '#ff5722', '#391409'],
-            baseColorBlue:   ['#303f9f', '#3f51b5', '#10142b'],
+            baseColorPurple:    ['#e040fb', '#9121b9', '#3d283d', '#b534cb'],
+            baseColorYellow:    ['#ffc107', '#b97400', '#413d0d', '#e48f00'],
+            baseColorGreen:     ['#8ed649', '#54762d', '#244627', '#6fa839'],
+            baseColorOrange:    ['#fa6306', '#c2421a', '#391409', '#d85605'],
+            baseColorBlue:      ['#6472ff', '#354596', '#151a38', '#3849c2'],
+            baseColorLightBlue: ['#87fcff', '#25717a', '#11393e', '#66bfc1'],
         };
-        this.colorSetKeys = ['--baseColor', '--logoColor', '--firstGradientColor'];
+        this.colorSetKeys = ['--baseColor', '--logoColor', '--firstGradientColor', '--logoHoverColor'];
 
         this.paintForm = document.getElementsByClassName('wrapper-block__change-color')[0];
         this.paintForm.addEventListener('mousedown', this.setRandomScheme.bind(this));
